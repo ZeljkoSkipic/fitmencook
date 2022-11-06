03e42658b0e3ac07cd887a792eec63bb2de837fb
zeljkoskipic <zeljko.skipic@gmail.com>
2022-11-06 17:07:43 +0100

    ACF Local JSON

--- functions.php
+++ functions.php
@@ -268,3 +268,36 @@ function unset_url_field($fields){
        unset($fields['url']);
        return $fields;
 }
+
+
+// Set Up ACF Local JSON
+
+add_filter('acf/settings/save_json', 'my_acf_json_save_point');
+
+function my_acf_json_save_point( $path ) {
+
+    // update path
+    $path = get_stylesheet_directory() . '/acf-json';
+
+
+    // return
+    return $path;
+
+}
+
+add_filter('acf/settings/load_json', 'my_acf_json_load_point');
+
+function my_acf_json_load_point( $paths ) {
+
+    // remove original path (optional)
+    unset($paths[0]);
+
+
+    // append path
+    $paths[] = get_stylesheet_directory() . '/acf-json';
+
+
+    // return
+    return $paths;
+
+}
