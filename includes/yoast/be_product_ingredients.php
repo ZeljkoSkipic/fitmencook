0a0adf1da1c1c69eefc215777e2c51cab7f31029
zeljkoskipic <zeljko.skipic@gmail.com>
2023-08-18 18:23:24 +0200

    Tools - Instacart MR

--- includes/yoast/be_product_ingredients.php
+++ includes/yoast/be_product_ingredients.php
@@ -4,7 +4,8 @@ use \Yoast\WP\SEO\Generators\Schema\Abstract_Schema_Piece;
 use \Yoast\WP\SEO\Generators\Schema\Article;
 use \Yoast\WP\SEO\Config\Schema_IDs;
 
-class BE_Product_Ingredients extends Article {
+class BE_Product_Ingredients extends Article
+{
 	/**
 	 * A value object with context variables.
 	 *
@@ -17,63 +18,140 @@ class BE_Product_Ingredients extends Article {
 	 *
 	 * @return bool
 	 */
-	public function is_needed() {
-		$post_types = apply_filters( 'be_product_ingredients_schema_post_types', array('recipes') );
-		if( is_singular( $post_types ) ) {
+	public function is_needed()
+	{
+		$post_types = apply_filters('be_product_ingredients_schema_post_types', array('recipes'));
+		if (is_singular($post_types)) {
 			return true;
 		}
 
-        return false;
+		return false;
 	}
 
-	public function generate() {
-		$post          = get_post( $this->context->id );
-        $post_type = get_post_type($this->context->id);
+	public function generate()
+	{
+		$post          = get_post($this->context->id);
+		$post_type = get_post_type($this->context->id);
 
 		$data          = array(
 			'@type'            => 'Recipe',
 			'@id'              => $this->context->canonical . '#recipe',
-			'isPartOf'         => array( '@id' => $this->context->canonical . Schema_IDs::ARTICLE_HASH ),
-			'name'         => wp_strip_all_tags( get_the_title() ),
-			'description' => wp_strip_all_tags( get_the_excerpt( $post ) ),
+			'isPartOf'         => array('@id' => $this->context->canonical . Schema_IDs::ARTICLE_HASH),
+			'name'         => wp_strip_all_tags(get_the_title()),
+			'description' => wp_strip_all_tags(get_the_excerpt($post)),
 			'author'           => array(
-				'@id'  => get_author_posts_url( get_the_author_meta( 'ID' ) ),
-				'name' => get_the_author_meta( 'display_name', $post->post_author ),
+				'@id'  => get_author_posts_url(get_the_author_meta('ID')),
+				'name' => get_the_author_meta('display_name', $post->post_author),
 			),
-			'publisher'        => array( '@id' => $this->get_publisher_url() ),
-			'datePublished'    => mysql2date( DATE_W3C, $post->post_date_gmt, false ),
-			'dateModified'     => mysql2date( DATE_W3C, $post->post_modified_gmt, false ),
+			'publisher'        => array('@id' => $this->get_publisher_url()),
+			'datePublished'    => mysql2date(DATE_W3C, $post->post_date_gmt, false),
+			'dateModified'     => mysql2date(DATE_W3C, $post->post_modified_gmt, false),
 			'mainEntityOfPage' => $this->context->canonical . Schema_IDs::WEBPAGE_HASH,
 		);
 
-        $ingredients_groups = get_field('ing_group', get_the_ID());
-        $steps = get_field('steps', get_the_ID());
+		$template  = get_page_template_slug(get_the_ID());
 
-        if($ingredients_groups) {
-            foreach($ingredients_groups as $ingredients_group) {
-				if($ingredients_group['ingredients_instacart']) {
-					foreach($ingredients_group['ingredients_instacart'] as $ingredient) {
-						if( $ingredient['ingredient']) {
-							$data['recipeIngredient'][] = $ingredient['ingredient'];
+		if ($template === 'single-recipes-multiple.php') {
+
+			// Existing Recipes
+
+			$existing_recipes = get_field('existing_recipe', get_the_ID());
+
+			if ($existing_recipes) {
+				foreach ($existing_recipes as $recipe) {
+					if (isset($recipe['recipe'][0])) {
+						$ingredients_groups_multiple = get_field('ing_group', $recipe['recipe'][0]->ID);
+						$include_steps = $recipe['include_steps'];
+
+						if ($include_steps) {
+							$steps = get_field('steps', $recipe['recipe'][0]->ID);
+							if ($steps) {
+								foreach ($steps as $step) {
+									$data['recipeInstructions'][] =  wp_strip_all_tags($step['step']);
+								}
+							}
 						}
-						
-						if($ingredient['substitution']) {
-							$data['recipeIngredient'][] = $ingredient['substitution'];
+
+
+						if ($ingredients_groups_multiple) {
+							foreach ($ingredients_groups_multiple as $ingredients_group) {
+								if ($ingredients_group['ingredients_instacart']) {
+									foreach ($ingredients_group['ingredients_instacart'] as $ingredient) {
+										if ($ingredient['ingredient']) {
+											$data['recipeIngredient'][] = $ingredient['ingredient'];
+										}
+
+										if ($ingredient['substitution']) {
+											$data['recipeIngredient'][] = $ingredient['substitution'];
+										}
+									}
+								}
+							}
 						}
-						
 					}
 				}
-               
-            }
-        }
+			}
 
-        if($steps) {
-            foreach($steps as $step) {
-                $data['recipeInstructions'][] =  wp_strip_all_tags( $step['step']);
-            }
-        }
+			// Custom Recipes
 
-		$data = apply_filters( 'be_ingredients_schema_data', $data, $this->context );
+			$custom_recipes = get_field('custom_recipe', get_the_ID());
+
+			if ($custom_recipes) {
+				foreach ($custom_recipes as $custom_recipe) {
+
+					$ing_groups = isset($custom_recipe['ing_group']) ?  $custom_recipe['ing_group'] : "";
+					$custom_recipes_steps = isset($custom_recipe['cr_steps']) ?  $custom_recipe['cr_steps'] : "";
+
+					if($custom_recipes_steps) {
+						foreach ($custom_recipes_steps as $custom_recipes_step) {
+							$data['recipeInstructions'][] =  wp_strip_all_tags($custom_recipes_step['cr_step']);
+						}
+					}
+
+					if ($ing_groups) {
+						foreach ($ing_groups as $ing_group) {
+							if ($ing_group['ingredients_instacart']) {
+								foreach ($ing_group['ingredients_instacart'] as $ingredient) {
+									if ($ingredient['ingredient']) {
+										$data['recipeIngredient'][] = $ingredient['ingredient'];
+									}
+
+									if ($ingredient['substitution']) {
+										$data['recipeIngredient'][] = $ingredient['substitution'];
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		} else {
+			$ingredients_groups = get_field('ing_group', get_the_ID());
+			$steps = get_field('steps', get_the_ID());
+			if ($ingredients_groups) {
+				foreach ($ingredients_groups as $ingredients_group) {
+					if ($ingredients_group['ingredients_instacart']) {
+						foreach ($ingredients_group['ingredients_instacart'] as $ingredient) {
+							if ($ingredient['ingredient']) {
+								$data['recipeIngredient'][] = $ingredient['ingredient'];
+							}
+
+							if ($ingredient['substitution']) {
+								$data['recipeIngredient'][] = $ingredient['substitution'];
+							}
+						}
+					}
+				}
+			}
+
+			if ($steps) {
+				foreach ($steps as $step) {
+					$data['recipeInstructions'][] =  wp_strip_all_tags($step['step']);
+				}
+			}
+
+			$data = apply_filters('be_ingredients_schema_data', $data, $this->context);
+		}
 
 		return $data;
 	}
@@ -83,12 +161,12 @@ class BE_Product_Ingredients extends Article {
 	 *
 	 * @return string
 	 */
-	private function get_publisher_url() {
-		if ( $this->context->site_represents === 'person' ) {
+	private function get_publisher_url()
+	{
+		if ($this->context->site_represents === 'person') {
 			return $this->context->site_url . Schema_IDs::PERSON_HASH;
 		}
 
 		return $this->context->site_url . Schema_IDs::ORGANIZATION_HASH;
 	}
-
 }
